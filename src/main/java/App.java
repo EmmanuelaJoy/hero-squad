f206e50f1d7c57ddad6b5895e542de5e3946b918
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-23 12:55:09 +0300

    add a get method in main class App that renders the about page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,5 +16,10 @@ public class App {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/about", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "about.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
