3c3cdfa5fbc73d00cabc467d7039c2de88f56669
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-23 11:06:24 +0300

    add member variables to class Squads

--- src/main/java/models/Squads.java
+++ src/main/java/models/Squads.java
@@ -1,4 +1,12 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squads {
+    private String name;
+    private String cause;
+    private int maxSize;
+    private ArrayList<Heroes> heroes;
+    private static List<Squads> squadInstances = new ArrayList<>();
 }
