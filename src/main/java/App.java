585cf149801a5c7992f1e720bfd64ff37d148ad5
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-23 12:28:05 +0300

    add required imports in App.java class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
+import static spark.Spark.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import models.Heroes;
+import models.Squads;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+    }
 }
