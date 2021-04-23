5e0a8c8a5e0627b0df9a4b39b963115a0909dfe7
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-23 08:48:07 +0300

    add member variables to class Heroes

--- src/main/java/models/Heroes.java
+++ src/main/java/models/Heroes.java
@@ -1,4 +1,15 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Heroes {
+
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private static ArrayList<Heroes> instances = new ArrayList<>();
+    private static int id;
+    private boolean squadMember;
+
 }
