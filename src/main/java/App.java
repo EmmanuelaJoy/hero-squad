290f8842b73a866a7063cb5476897c58170f66da
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-23 12:49:43 +0300

    add a get method in main class App that renders the homepage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,5 +12,9 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
